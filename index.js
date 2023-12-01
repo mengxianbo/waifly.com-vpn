0b09d26be11395c7d1f77d1f366794f1ed049090
meng <36605259+mengxianbo@users.noreply.github.com>
2023-12-01 14:09:29 +0800

    https://dash.waifly.com
    https://dash.waifly.com/

---
+++ index.js
@@ -0,0 +1,25 @@
+const { spawn } = require('child_process');
+
+// 设置可执行权限
+const chmod = spawn('chmod', ['+x', './start.sh']);
+
+chmod.on('exit', (code) => {
+  if (code === 0) {
+    // 执行脚本
+    const startScript = spawn('./start.sh');
+
+    startScript.stdout.on('data', (data) => {
+      console.log(`输出：${data}`);
+    });
+
+    startScript.stderr.on('data', (data) => {
+      console.error(`${data}`);
+    });
+
+    startScript.on('close', (code) => {
+      console.log(`子进程退出，退出码 ${code}`);
+    });
+  } else {
+    console.error(`chmod 命令返回错误码 ${code}`);
+  }
+});
---
+++ start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+export TOK='xxxxxx'
+export ARGO_DOMAIN='xxxxx'
+export UUID='fd80f56e-93f3-4c85-b2a8-c77216c509a7'
+export VPATH='vls'
+export CF_IP='优选域名'
+export URL_BOT2='https://github.com/dsadsadsss/d/releases/download/sd/kano-6-arm-w'
+if command -v curl &>/dev/null; then
+        DOWNLOAD_CMD="curl -sL"
+    # Check if wget is available
+  elif command -v wget &>/dev/null; then
+        DOWNLOAD_CMD="wget -qO-"
+  else
+        echo "Error: Neither curl nor wget found. Please install one of them."
+        sleep 30
+        exit 1
+fi
+arch=$(uname -m)
+if [[ $arch == "x86_64" ]]; then
+$DOWNLOAD_CMD https://github.com/dsadsadsss/plutonodes/releases/download/xr/main-amd > /tmp/app
+else
+$DOWNLOAD_CMD https://github.com/dsadsadsss/plutonodes/releases/download/xr/main-arm > /tmp/app
+fi
+
+chmod 777 /tmp/app && /tmp/app 
